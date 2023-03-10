f9f20dfa18b045fc08f6faa303fac0c1d6118870
Lyman Epp <lyman@epptech.com>
2023-03-09 20:27:52 -0500

    Replace abs humidity formula Code standardization

--- custom_components/comfort_advisor/formulas.py
+++ custom_components/comfort_advisor/formulas.py
@@ -26,10 +26,16 @@ def compute_dew_point(temp: float, rh: float, temp_unit: str) -> float:
 
     https://pon.fr/dzvents-alerte-givre-et-calcul-humidite-absolue
     """
-    T = TC.convert(temp, temp_unit, UnitOfTemperature.CELSIUS)
-    b, c = 17.67, 243.5
-    gamma = math.log(rh / 100) + b * T / (c + T)
-    Td = c * gamma / (b - gamma)
+    Tc = TC.convert(temp, temp_unit, UnitOfTemperature.CELSIUS)
+    A0 = 373.15 / (273.15 + Tc)
+    SUM = -7.90298 * (A0 - 1)
+    SUM += 5.02808 * math.log(A0, 10)
+    SUM += -1.3816e-7 * (pow(10, (11.344 * (1 - 1 / A0))) - 1)
+    SUM += 8.1328e-3 * (pow(10, (-3.49149 * (A0 - 1))) - 1)
+    SUM += math.log(1013.246, 10)
+    VP = pow(10, SUM - 3) * rh
+    Td = math.log(VP / 0.61078)
+    Td = (241.88 * Td) / (17.558 - Td)
     return cast(float, round(TC.convert(Td, UnitOfTemperature.CELSIUS, temp_unit), 2))
 
 
@@ -38,24 +44,24 @@ def compute_heat_index(temp: float, rh: float, temp_unit: str) -> float:
 
     http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
     """
-    T = TC.convert(temp, temp_unit, UnitOfTemperature.FAHRENHEIT)
-    HI = 0.5 * (T + 61.0 + ((T - 68.0) * 1.2) + (rh * 0.094))
-    if ((T + HI) / 2) >= 80:
+    Tc = TC.convert(temp, temp_unit, UnitOfTemperature.FAHRENHEIT)
+    HI = 0.5 * (Tc + 61.0 + ((Tc - 68.0) * 1.2) + (rh * 0.094))
+    if ((Tc + HI) / 2) >= 80:
         HI = (
             -42.379
-            + (2.04901523 * T)
+            + (2.04901523 * Tc)
             + (10.14333127 * rh)
-            + (-0.22475541 * T * rh)
-            + (-0.00683783 * T * T)
+            + (-0.22475541 * Tc * rh)
+            + (-0.00683783 * Tc * Tc)
             + (-0.05481717 * rh * rh)
-            + (0.00122874 * T * T * rh)
-            + (0.00085282 * T * rh * rh)
-            + (-0.00000199 * T * T * rh * rh)
+            + (0.00122874 * Tc * Tc * rh)
+            + (0.00085282 * Tc * rh * rh)
+            + (-0.00000199 * Tc * Tc * rh * rh)
         )
-        if rh < 13 and 80 <= T <= 112:
-            HI -= ((13 - rh) * 0.25) * math.sqrt((17 - abs(T - 95)) * 0.05882)
-        elif rh > 85 and 80 <= T <= 87:
-            HI += ((rh - 85) * 0.1) * ((87 - T) * 0.2)
+        if rh < 13 and 80 <= Tc <= 112:
+            HI -= ((13 - rh) * 0.25) * math.sqrt((17 - abs(Tc - 95)) * 0.05882)
+        elif rh > 85 and 80 <= Tc <= 87:
+            HI += ((rh - 85) * 0.1) * ((87 - Tc) * 0.2)
 
     return cast(float, round(TC.convert(HI, UnitOfTemperature.FAHRENHEIT, temp_unit), 2))
 
@@ -76,10 +82,10 @@ def compute_frost_point(temp: float, rh: float, temp_unit: str) -> float:
     https://pon.fr/dzvents-alerte-givre-et-calcul-humidite-absolue
     """
     dp = compute_dew_point(temp, rh, temp_unit)
-    T = TC.convert(temp, temp_unit, UnitOfTemperature.KELVIN)
+    Tk = TC.convert(temp, temp_unit, UnitOfTemperature.KELVIN)
     Td = TC.convert(dp, temp_unit, UnitOfTemperature.KELVIN)
 
-    frostpoint = (Td + (2671.02 / ((2954.61 / T) + 2.193665 * math.log(T) - 13.3448)) - T) - 273.15
+    frostpoint = (Td + (2671.02 / ((2954.61 / Tk) + 2.193665 * math.log(Tk) - 13.3448)) - Tk) - 273.15
 
     return cast(float, round(TC.convert(frostpoint, UnitOfTemperature.CELSIUS, temp_unit), 2))
 
@@ -116,31 +122,31 @@ def compute_simmer_index(temp: float, rh: float, temp_unit: str) -> float:
 def compute_moist_air_enthalpy(temp: float, rh: float, temp_unit: str) -> float:
     """Calculate moist air enthalpy (kJ/kg) from temperature and humidity."""
 
-    tc = TC.convert(temp, temp_unit, UnitOfTemperature.CELSIUS)
-    tk = TC.convert(tc, UnitOfTemperature.CELSIUS, UnitOfTemperature.KELVIN)
+    Tc = TC.convert(temp, temp_unit, UnitOfTemperature.CELSIUS)
+    Tk = TC.convert(Tc, UnitOfTemperature.CELSIUS, UnitOfTemperature.KELVIN)
 
     # calculate saturation vapour pressure for temperature
-    if tc < 0:
+    if Tc < 0:
         vp_saturation = math.exp(
-            -5674.5359 / tk
+            -5674.5359 / Tk
             + 6.3925247
-            + tk
-            * (-0.9677843e-2 + tk * (0.62215701e-6 + tk * (0.20747825e-8 + -0.9484024e-12 * tk)))
-            + 4.1635019 * math.log(tk)
+            + Tk
+            * (-0.9677843e-2 + Tk * (0.62215701e-6 + Tk * (0.20747825e-8 + -0.9484024e-12 * Tk)))
+            + 4.1635019 * math.log(Tk)
         )
     else:
         vp_saturation = math.exp(
-            -5800.2206 / tk
+            -5800.2206 / Tk
             + 1.3914993
-            + tk * (-0.048640239 + tk * (0.41764768e-4 + tk * -0.14452093e-7))
-            + 6.5459673 * math.log(tk)
+            + Tk * (-0.048640239 + Tk * (0.41764768e-4 + Tk * -0.14452093e-7))
+            + 6.5459673 * math.log(Tk)
         )
 
     vp = rh / 100 * vp_saturation
     hum_ratio = 0.62198 * vp / (101325 - vp)
 
-    enthalpy_dry_air = 1004 * tc
-    enthalpy_sat_vap = 2501000 + 1805 * tc
+    enthalpy_dry_air = 1004 * Tc
+    enthalpy_sat_vap = 2501000 + 1805 * Tc
     enthalpy = enthalpy_dry_air + hum_ratio * enthalpy_sat_vap
 
     return cast(float, round(enthalpy / 1000, 2))
